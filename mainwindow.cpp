ac26cfff6126c59d625f0fe8f24a1a64b61d24da
Mateus Nascimento <mateus.nnascimento.s@gmail.com>
2017-05-07 18:22:05 -0300

    Primeira versão da tela principal

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,4 +1,5 @@
 #include "mainwindow.h"
+#include "lancamentowindow.h"
 #include "ui_mainwindow.h"
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -6,9 +7,22 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setStyleSheet("background-color: white;");
+    this->setFixedSize(this->width(), this->height());
+    QPixmap pix = QPixmap("logo.png");
+    pix = pix.scaled(200, 200, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+    ui->logo->setPixmap(pix);
+    QHeaderView* header = ui->tableWidget->horizontalHeader();
+    header->setSectionResizeMode(QHeaderView::Stretch);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_btn_lancamento_clicked()
+{
+    LancamentoWindow *lw = new LancamentoWindow();
+    lw->show();
 }
